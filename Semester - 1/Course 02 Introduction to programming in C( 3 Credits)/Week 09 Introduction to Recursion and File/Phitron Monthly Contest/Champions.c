9e20874397b1496bb5e9a1fd50798090abb64e07
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2022-12-24 11:26:26 +0600

    Champions problem solved

--- Semester - 1/Course 02 Introduction to programming in C( 3 Credits)/Week 09 Introduction to Recursion and File/Phitron Monthly Contest/Champions.c
+++ Semester - 1/Course 02 Introduction to programming in C( 3 Credits)/Week 09 Introduction to Recursion and File/Phitron Monthly Contest/Champions.c
@@ -6,24 +6,20 @@ int main()
     char str[1000];
     scanf("%s", str);
 
-    char word[] = "champions";
-
-    int i, x = 0;
+    int i, flag = 1;
     for(i = 0; str[i] != '\0'; i++) {
         if(str[i] >= 'A' && str[i] <= 'Z') {
             str[i] += 32;
         }
     }
     for(i = 0; str[i] != '\0'; i++) {
-        if(str[i] == word[x]) {
-            x++;
+        if(str[i] == 'c' && str[i+1] == 'h' && str[i+2] == 'a' && str[i+3] == 'm' && str[i+4] == 'p' && str[i+5] == 'i' && str[i+6] == 'o' && str[i+7] == 'n' && str[i+8] == 's') {
+            printf("Yes\n");
+            flag = 0;
+            break;
         }
     }
-    if(x == 9) {
-        printf("Yes\n");
-    }
-    else {
-        printf("No\n");
-    }
+    if(flag) printf("No\n");
+
     return 0;
 }
