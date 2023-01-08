63207f9ceb32996c80cce356f2f4143132845051
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-01-08 18:14:55 +0600

    cf 1608 solved

--- Semester - 1/Course 02 Introduction to programming in C( 3 Credits)/Week 07 Introduction to pointer & recursion/Module 25.5/Codeforces - 1608A.c
+++ Semester - 1/Course 02 Introduction to programming in C( 3 Credits)/Week 07 Introduction to pointer & recursion/Module 25.5/Codeforces - 1608A.c
@@ -2,5 +2,20 @@
 
 int main()
 {
-
+    int t, i;
+    scanf("%d", &t);
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+        if(n == 1) {
+            printf("%d\n", n);
+        }
+        else if(n > 1) {
+            for(i = 2; i <= n+1; i++) {
+                printf("%d ", i);
+            }
+            printf("\n");
+        }
+    }
+    return 0;
 }
---
+++ Semester - 1/Course 02 Introduction to programming in C( 3 Credits)/Week 07 Introduction to pointer & recursion/Module 25.5/Codeforces - 1680A.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    while(t--) {
+        int l1, r1, l2, r2;
+        scanf("%d %d %d %d", &l1, &r1, &l2, &r2);
+    }
+}
