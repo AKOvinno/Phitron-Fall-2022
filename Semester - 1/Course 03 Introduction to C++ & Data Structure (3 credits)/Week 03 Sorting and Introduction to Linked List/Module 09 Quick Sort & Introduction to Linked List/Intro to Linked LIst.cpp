f768257568ea68cfffb6502d356cdd3fb60708ec
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-02-22 19:02:39 +0600

    module 09 quick sort added

---
+++ Semester - 1/Course 03 Introduction to C++ & Data Structure (3 credits)/Week 03 Sorting and Introduction to Linked List/Module 09 Quick Sort & Introduction to Linked List/Intro to Linked LIst.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class
+{
+public:
+    int data;
+    int *ptr;
+};
+
+int main()
+{
+
+    return 0;
+}
--- Semester - 1/Course 03 Introduction to C++ & Data Structure (3 credits)/Week 03 Sorting and Introduction to Linked List/Module 10 Lab Class 03/Linked List Structure implementation.cpp
+++ Semester - 1/Course 03 Introduction to C++ & Data Structure (3 credits)/Week 03 Sorting and Introduction to Linked List/Module 10 Lab Class 03/Linked List Structure implementation.cpp
@@ -7,33 +7,75 @@ public:
     int data;
     node* nxt;
 };
-
 class LinkedList
 {
-    public:
+public:
     node* head;
+
+    // constructor
     LinkedList()
     {
         head = NULL;
     }
-    void InsertAtHead()
+    // Creates a new node with data = value and next = NULL
+    node* CreateNewNode(int value)
     {
-
+        node *newnode = new node;
+        newnode -> data = value;
+        newnode -> nxt = NULL;
+        return newnode;
     }
+    // Insert new value at Head
+    void InsertAtHead(int value)
+    {
+        node *a = CreateNewNode(value);
+        if(head == NULL) {
+            head = a;
+            return;
+        }
+        // If head is not NULL
+        a -> nxt = head;
+        head = a;
+    }
+    // prints the linked list
     void Traverse()
     {
-
+        node *a = head;
+        cout << "Linked List Traverse: ";
+        while(a != NULL) {
+            cout << a -> data << " ";
+            a = a -> nxt;
+        }
+        cout << "\n";
     }
-    void SearchDistinctValue()
+    // Search for a single value
+    int SearchDistinctValue(int value)
     {
-
+        node* a = head;
+        int index = 0;
+        while(a != NULL) {
+            if(a -> data == value) {
+                return index;
+            }
+            a = a -> nxt;
+            index++;
+        }
+        return -1;
     }
+    // Search all possible occurrance
     void SearchAllValue(int value)
     {
-
+        node* a = head;
+        int index = 0;
+        while(a != NULL) {
+            if(a -> data == value) {
+                cout << value << " is found at index " << index << "\n";
+            }
+            a = a -> nxt;
+            index++;
+        }
     }
 };
-
 int main()
 {
     LinkedList l;
@@ -44,11 +86,9 @@ int main()
     l.InsertAtHead(30);
 
     l.Traverse();
-
-    l.SearchDistinctValue();
-    l.SearchDistinctValue(5);
-
-    l.SearchAllValue(10);
-
+    cout << "10 is found at " << l.SearchDistinctValue(10)+1 << "\n";
+    cout << "5 is found at " << l.SearchDistinctValue(5) << "\n";
+    cout << "30 is found at " << l.SearchDistinctValue(30) << "\n";
+    l.SearchAllValue(30);
     return 0;
 }
