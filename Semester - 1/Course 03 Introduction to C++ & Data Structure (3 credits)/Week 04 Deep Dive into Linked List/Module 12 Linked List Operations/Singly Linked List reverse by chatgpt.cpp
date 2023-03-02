bfdbc296363ce976e4b13d1da9c793c3fc58e5ce
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-03-02 22:14:35 +0600

    singly linked list reverse added

---
+++ Semester - 1/Course 03 Introduction to C++ & Data Structure (3 credits)/Week 04 Deep Dive into Linked List/Module 12 Linked List Operations/Singly Linked List reverse by chatgpt.cpp
@@ -0,0 +1,100 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Creation of node type object
+class node
+{
+public:
+    int data;
+    node* nxt;
+};
+class LinkedList
+{
+public:
+    node* head;
+
+    // constructor
+    LinkedList()
+    {
+        head = NULL;
+    }
+    // Creates a new node with data = value and next = NULL
+    node* CreateNewNode(int value)
+    {
+        node* newnode = new node;
+        newnode -> data = value;
+        newnode -> nxt = NULL;
+        return newnode;
+    }
+    // Insert new value at tail
+    void InsertAtTail(int value)
+    {
+        node* newnode = CreateNewNode(value);
+        if(head == NULL) {
+            head = newnode;
+            return;
+        }
+        node* temp = head;
+        while(temp -> nxt != NULL) {
+            temp = temp -> nxt;
+        }
+        temp -> nxt = newnode;
+    }
+    // Insert new value at Head
+    void InsertAtHead(int value)
+    {
+        node *a = CreateNewNode(value);
+        if(head == NULL) {
+            head = a;
+            return;
+        }
+        // If head is not NULL
+        a -> nxt = head;
+        head = a;
+    }
+    // prints the linked list
+    void Traverse()
+    {
+        node *a = head;
+        cout << "Linked List Traverse: ";
+        while(a != NULL) {
+            cout << a -> data << " ";
+            a = a -> nxt;
+        }
+        cout << "\n";
+    }
+    void Reverse()
+    {
+        node* current = head;
+        node* prev = NULL;
+        node* next = NULL;
+
+        while(current != NULL) {
+            //going ahead;
+            next = current -> nxt;
+            // reversing
+            current -> nxt = prev;
+            // swapping
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+};
+
+int main()
+{
+    LinkedList l;
+
+    l.InsertAtHead(10);
+    l.InsertAtHead(20);
+    l.InsertAtHead(30);
+    l.InsertAtHead(40);
+    l.InsertAtHead(50);
+    l.Traverse();
+    l.Reverse();
+    l.Traverse();
+    return 0;
+}
+
+
