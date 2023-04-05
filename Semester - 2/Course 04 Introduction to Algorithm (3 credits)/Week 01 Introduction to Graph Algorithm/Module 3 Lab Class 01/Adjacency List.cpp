4052317c5e3a923de5fa719a78d1d51851fa7995
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-04-05 20:04:50 +0600

    adjacency list added

--- Semester - 2/Course 04 Introduction to Algorithm (3 credits)/Week 01 Introduction to Graph Algorithm/Module 3 Lab Class 01/Adjacency List.cpp
+++ Semester - 2/Course 04 Introduction to Algorithm (3 credits)/Week 01 Introduction to Graph Algorithm/Module 3 Lab Class 01/Adjacency List.cpp
@@ -35,16 +35,33 @@ Input:
 3 1
 2 3
 3 2
-
-Time complexity -> O(n^2)
-Space Complexity -> O(n^2)
-
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
+// Time complexity = O(n^2)
+// Space complexity = O(n^2)
+
 int main()
 {
+    int nodes = 4;
+    vector <int> adj_list[nodes];
 
+    // O(E * 2) = (E) // Worst Case = O(n^2)
+    adj_list[0] = {1};
+    adj_list[1] = {0, 2, 3};
+    adj_list[2] = {1, 3};
+    adj_list[3] = {1, 2};
+
+    // O(2 * E) = O(E) // Worst Case = O(n^2)
+    // Space Complexity O(1)
+    for(int i = 0; i < nodes; i++) {
+        cout << i << " -> ";
+        for(int j = 0; j < adj_list[i].size(); j++) {
+            cout << adj_list[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
 }
