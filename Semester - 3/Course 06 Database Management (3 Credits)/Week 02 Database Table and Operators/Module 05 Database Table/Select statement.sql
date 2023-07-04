8bf2f4157fcddeec63efb9f38d64ca8e79ef7e28
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-07-04 14:55:43 +0600

    module 6 and 6.5 added

--- Semester - 3/Course 06 Database Management (3 Credits)/Week 02 Database Table and Operators/Module 05 Database Table/Select statement.sql
+++ Semester - 3/Course 06 Database Management (3 Credits)/Week 02 Database Table and Operators/Module 05 Database Table/Select statement.sql
@@ -1,30 +1,3 @@
-CREATE TABLE Student (
-    Roll INT,
-    Name VARCHAR(30),
-    ContactNo CHAR(11),
-    Email VARCHAR(30)
-);
-
-INSERT INTO Student (Roll, Name, ContactNo, Email)
-VALUES(101, "Rahim", "01712243640", "rahim@gmail.com");
-INSERT INTO Student (Roll, Name, ContactNo, Email)
-VALUES(102, "Karim", "01612243720", "karim@gmail.com");
-INSERT INTO Student (Roll, Name, ContactNo, Email)
-VALUES(103, "Rahim", "01722489073", "rh@gmail.com");
-INSERT INTO Student (Roll, Name, Email)
-VALUES(104, "Ovinno", "rh@gmail.com");
-INSERT INTO Student (Roll, Name, ContactNo, Email)
-VALUES(105, "Laila", "01791706859", "rh@gmail.com");
-
-#find everything
-SELECT * 
-FROM Student;
-
-#find columns
-SELECT Roll, Email
-FROM Student;
-
-#find records
-SELECT * #star means all the columns will be shown
-FROM Student
-WHERE Roll = 105
+SELECT first_name, salary
+FROM employees
+WHERE salary BETWEEN 9000 and 15000
