fe1fa3837428e8976d1985402110e47d549675d2
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-08-25 22:47:45 +0600

    random

--- Semester - 3/Course 07 Software Engineering Project (2 Credits)/Week 03 Class Based View and REST API/Module 09 Class Based View/book_store_management_system/book/urls.py
+++ Semester - 3/Course 07 Software Engineering Project (2 Credits)/Week 03 Class Based View and REST API/Module 09 Class Based View/book_store_management_system/book/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from book.views import store_book, show_book, edit_book, delete_book
-from . import views
+from book.views import store_book, edit_book, delete_book, HomeTemplateView, BookListView
 
 urlpatterns = [
-    path('<int:roll>/', views.HomeTemplateView.as_view(), {'founder': 'Laila'}, name='homepage'),
+    path('', HomeTemplateView.as_view(), name='homepage'),
+    #path('<int:roll>/', HomeTemplateView.as_view(), {'founder': 'Laila'}, name='homepage'),
     path('store_new_book/', store_book, name='store_book'),
-    path('show_books/', show_book, name='show_book'),
+    #path('show_books/', show_book, name='show_book'),
+    path('show_books/', BookListView.as_view(), name='show_book'),
     path('edit_books/<int:id>', edit_book, name='edit_book'),
     path('delete_book/<int:id>', delete_book, name='delete_book'),
 ]
--- Semester - 3/Course 07 Software Engineering Project (2 Credits)/Week 03 Class Based View and REST API/Module 09 Class Based View/book_store_management_system/book/views.py
+++ Semester - 3/Course 07 Software Engineering Project (2 Credits)/Week 03 Class Based View and REST API/Module 09 Class Based View/book_store_management_system/book/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from book.forms import BookStoreForm
 from book.models import BookStoreModel
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 # Create your views here.
 
 # function based view
@@ -31,9 +31,40 @@ def store_book(request):
         book = BookStoreForm()
     return render(request, 'store_book.html', {'form' : book})
 
-def show_book(request):
-    book = BookStoreModel.objects.all()
-    return render(request, 'show_book.html', {'data': book})
+# def show_book(request):
+#     book = BookStoreModel.objects.all()
+#     return render(request, 'show_book.html', {'data': book})
+class BookListView(ListView):
+    # here model is a builtin variable
+    model = BookStoreModel
+    template_name = 'show_book.html'
+    context_object_name = 'book_list'
+    
+    # def get_queryset(self):
+    #     return BookStoreModel.objects.filter(id = '1')
+    
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context = {'Humayun': BookStoreModel.objects.filter(author='Humayun Ahmed')}
+    #     return context
+    
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context = {'Humayun': BookStoreModel.objects.all().order_by('author')}
+    #     return context
+    
+    ordering = ['-id']
+    
+    # Creating separate view for separate users
+    # def get_template_names(self): # It will override template
+    #     if self.request.user.is_superuser:
+    #         template_name = 'superuser.html'
+    #     elif self.request.user.is_staff:
+    #         template_name = 'staffuser.html'
+    #     else:
+    #         template_name = self.template_name
+    #     return [template_name]
+        
 
 def edit_book(request, id):
     book = BookStoreModel.objects.get(pk = id)
