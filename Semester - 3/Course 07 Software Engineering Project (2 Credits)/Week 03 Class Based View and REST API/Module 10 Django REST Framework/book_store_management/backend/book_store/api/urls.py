cd6e4a1ec335f7978f45f20c85aa5a1543c33167
Ashfaq Kadir Ovinno <akovinno.official@gmail.com>
2023-08-31 00:47:03 +0600

    module 10 completed fully

--- Semester - 3/Course 07 Software Engineering Project (2 Credits)/Week 03 Class Based View and REST API/Module 10 Django REST Framework/book_store_management/backend/book_store/api/urls.py
+++ Semester - 3/Course 07 Software Engineering Project (2 Credits)/Week 03 Class Based View and REST API/Module 10 Django REST Framework/book_store_management/backend/book_store/api/urls.py
@@ -1,14 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from api.views import BookListView, BookListUpdateDelete
+from api.views import BookViewSet
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
-router.register(r'books', BookListView, basename="book")
+router.register(r'books', BookViewSet, basename="book")
 # router.register(r'users', views.UserViewSet,basename="user")
 
 # The API URLs are now determined automatically by the router.
 urlpatterns = [
-    path('books/', BookListView.as_view()), # handling get and post request
-    path('books/<int:pk>/', BookListUpdateDelete.as_view()), 
+    # Method - 1
+    # path('books/', BookListView.as_view()), # handling get and post request
+    # path('books/<int:pk>/', BookListUpdateDelete.as_view()), 
+    
+    # Method - 2
+    # path('books/', BookListCreateAPIView.as_view()), # handling get and post request
+    # path('books/<int:pk>/', BookRetrieveUpdateDestroyAPIView.as_view()), 
+    
+    # Method - 3
+    path('', include(router.urls))
+    
 ]
--- Semester - 3/Course 07 Software Engineering Project (2 Credits)/Week 03 Class Based View and REST API/Module 10 Django REST Framework/book_store_management/backend/book_store/api/views.py
+++ Semester - 3/Course 07 Software Engineering Project (2 Credits)/Week 03 Class Based View and REST API/Module 10 Django REST Framework/book_store_management/backend/book_store/api/views.py
@@ -1,51 +1,77 @@
 from api.models import BookStoreModel
 from api.serializers import BookStoreSerializer
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 
+# from django.http import Http404
+# from rest_framework.views import APIView
+# from rest_framework.response import Response
+# from rest_framework import status
 
-class BookListView(APIView):
-    """
-    List all model_data, or create a new model_data.
-    """
-    def get(self, request, format=None):
-        model_data = BookStoreModel.objects.all() # bringing model data
-        serializer = BookStoreSerializer(model_data, many=True) # converting the model data into serializer
-        return Response(serializer.data)
+# Method - 1: API View
 
-    def post(self, request, format=None):
-        serializer = BookStoreSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class BookListView(APIView):
+#     """
+#     List all model_data, or create a new model_data.
+#     """
+#     def get(self, request, format=None):
+#         model_data = BookStoreModel.objects.all() # bringing model data
+#         serializer = BookStoreSerializer(model_data, many=True) # converting the model data into serializer
+#         return Response(serializer.data)
+
+#     def post(self, request, format=None):
+#         serializer = BookStoreSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-class BookListUpdateDelete(APIView):
+# class BookListUpdateDelete(APIView):
+#     """
+#     Retrieve, update or delete a model_data instance.
+#     """
+#     def get_object(self, pk):
+#         try:
+#             return BookStoreModel.objects.get(pk=pk)
+#         except BookStoreModel.DoesNotExist:
+#             raise Http404
+
+#     def get(self, request, pk, format=None):
+#         model_data = self.get_object(pk)
+#         serializer = BookStoreSerializer(model_data)
+#         return Response(serializer.data)
+
+#     def put(self, request, pk, format=None):
+#         model_data = self.get_object(pk)
+#         serializer = BookStoreSerializer(model_data, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+#     def delete(self, request, pk, format=None):
+#         model_data = self.get_object(pk)
+#         model_data.delete()
+#         return Response(status=status.HTTP_204_NO_CONTENT)
+
+# Method - 2: Concrete API View
+
+# from rest_framework import generics
+
+# class BookListCreateAPIView(generics.ListCreateAPIView): # get, post request
+#     queryset = BookStoreModel.objects.all()
+#     serializer_class = BookStoreSerializer
+
+
+# class BookRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView): # update, delete, single element access
+#     queryset = BookStoreModel.objects.all()
+#     serializer_class = BookStoreSerializer
+
+# Method - 3: Model View Set (Shortcut)
+
+from rest_framework import viewsets
+
+class BookViewSet(viewsets.ModelViewSet):
     """
-    Retrieve, update or delete a model_data instance.
+    This viewset automatically provides `list` and `retrieve` actions.
     """
-    def get_object(self, pk):
-        try:
-            return BookStoreModel.objects.get(pk=pk)
-        except BookStoreModel.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        model_data = self.get_object(pk)
-        serializer = BookStoreSerializer(model_data)
-        return Response(serializer.data)
-
-    def put(self, request, pk, format=None):
-        model_data = self.get_object(pk)
-        serializer = BookStoreSerializer(model_data, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        model_data = self.get_object(pk)
-        model_data.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = BookStoreModel.objects.all()
+    serializer_class = BookStoreSerializer
